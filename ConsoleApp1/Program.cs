8a46ef2a255cd59f23cd7187e575771bd34c9d32
Peter Kiesel <p.kiesel@deskware.de>
2023-04-28 19:37:35 +0200

    Maui and Console

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Dw.WcfService;
+using System.ServiceModel;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var wcfClient = new ServiceClient(ServiceClient.EndpointConfiguration.BasicHttpBinding_IService,
+    new EndpointAddress("http://192.168.6.36:63489/Service.svc"));
+
+var s = wcfClient.GetData(100);
+
+Console.WriteLine(s);
+
